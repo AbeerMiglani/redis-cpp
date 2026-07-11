191ee8bc9ccecf7fa389c047c7c4a81fb11684f1
Abeer Miglani <216707591+AbeerMiglani@users.noreply.github.com>
2026-07-11 13:27:22 +0530

    Update redis_server.cpp

--- redis_server.cpp
+++ redis_server.cpp
@@ -1,3 +1,5 @@
+#include <cassert>
+#include <cstddef>
 #include <iostream>
 #include <unistd.h>
 #include <netinet/in.h>
@@ -49,6 +51,17 @@ int main (void)
             continue;
         }
 
+        while (true)
+        {
+            int32_t err = one_request(conn_fd);
+            if (err != 0)
+            {
+                break;
+            }
+        }
+
+        close(conn_fd);
+
         do_something(conn_fd);
         close(conn_fd);
         return 0;
@@ -68,4 +81,77 @@ static void do_something(int connfd)
 
     char wbuf[64] = "World";
     write(connfd, wbuf, strlen(wbuf));
+}
+
+static int32_t read_full (int fd, char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0)
+        {
+            return -1;
+        }
+        assert((size_t) rv <= n);
+        n -= (size_t) rv;
+        buf += rv;
+    }
+
+    return 0;
+}
+
+static int32_t write_all (int fd, const char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0)
+        {
+            return -1;
+        }
+        assert((size_t) rv <= n);
+        n -= (size_t) rv;
+        buf += rv;
+    }
+
+    return 0;
+}
+
+const size_t k_max_msg = 4096;
+
+static int32_t one_request(int connfd)
+{
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err)
+    {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg)
+    {
+        msg("too long");
+        return -1;
+    }
+
+    err = read_full(connfd, rbuf + 4, len);
+    if (err)
+    {
+        msg("read error");
+        return err;
+    }
+
+    std::cout << "Client says: " << std::string(rbuf + 4, len) << std::endl;
+
+    const char reply[] = "World";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t) strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+
+    return write_all(connfd, wbuf, 4 + len);
 }
