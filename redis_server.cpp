4efcd3f182ef12fdd391f508c695236fb680e8e1
Abeer Miglani <216707591+AbeerMiglani@users.noreply.github.com>
2026-07-12 19:09:33 +0530

    Update redis_server.cpp

--- redis_server.cpp
+++ redis_server.cpp
@@ -7,6 +7,11 @@
 #include <sys/socket.h>
 
 static void do_something(int connfd);
+static void msg (const std::string &message);
+static int32_t read_full (int fd, char *buf, size_t n);
+static int32_t write_all (int fd, const char *buf, size_t n);
+static int32_t one_request(int connfd);
+static int32_t query(int fd, const char *text);
 
 int main (void)
 {
@@ -154,4 +159,52 @@ static int32_t one_request(int connfd)
     memcpy(&wbuf[4], reply, len);
 
     return write_all(connfd, wbuf, 4 + len);
+}
+
+static int32_t query(int fd, const char *text)
+{
+    uint32_t len = (uint32_t) strlen(text);
+    if (len > k_max_msg)
+    {
+        return -1;
+    }
+
+    char wbuf[4 + k_max_msg];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+    if (int32_t err = write_all(fd, wbuf, 4 + len))
+    {
+        return err;
+    }
+
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err)
+    {
+        msg(errno == 0 ? "EOF" : "read() error");
+        return err;
+    }
+
+    memcpy(&len, rbuf, 4);
+    if (len > k_max_msg)
+    {
+        msg("too long");
+        return -1;
+    }
+
+    err = read_full(fd, &rbuf[4], len);
+    if (err)
+    {
+        msg("read() error");
+        return err;
+    }
+
+    std::cout << "Server says: " << std::string(rbuf + 4, len) << std::endl;
+    return 0;
+}
+
+static void msg (const std::string &message)
+{
+    std::cerr << message << std::endl;
 }
